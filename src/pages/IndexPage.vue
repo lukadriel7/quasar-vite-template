615f3bc9789fb72f5b5a1f0d97a3391aaaa1f315
Yozan Bi Adriel junior <lukadriel@gmail.com>
2022-04-27 14:19:21 +0000

    add use-mouse component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
   <q-page class="row items-center justify-evenly">
     {{ hello }}
     <q-btn label="Click me" @click="notify(`${level}`)"></q-btn>
+    <!-- <use-mouse v-slot="{ x, y }"> x: {{ x }}, y: {{ y }} </use-mouse> -->
   </q-page>
 </template>
 

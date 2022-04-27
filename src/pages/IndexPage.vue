cbdf587697f21932ed325ca02114dc3d01e35b99
Yozan Bi Adriel junior <lukadriel@gmail.com>
2022-04-27 13:57:52 +0000

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    {{ hello }}
+    <q-btn label="Click me" @click="notify(`${level}`)"></q-btn>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+const hello = ref('hello');
+const { notify } = useQuasar();
+const { level } = useBattery();
+</script>

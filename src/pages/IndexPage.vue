85456d395aab27a675d5b3cc8f6b97ec56fa16ea
Yozan Bi Adriel junior <lukadriel@gmail.com>
2022-05-02 17:02:06 +0000

    Revert "add basic card layout"
    This reverts commit 482b6616c702e35311535874605008ea6e742b9e.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,61 +1,13 @@
 <template>
-  <q-page padding>
-    <div class="column">
-      <div class="col">
-        <div class="row q-col-gutter-md">
-          <div class="col-3">
-            <div class="column q-gutter-md">
-              <div class="col">
-                <q-card>
-                  <q-card-section>
-                    <p>Congratulation</p>
-                  </q-card-section>
-                </q-card>
-              </div>
-              <div class="col row justify-between">
-
-                <q-card>
-                  <q-card-section>
-                    <p>hello</p>
-                  </q-card-section>
-                </q-card>
-                <q-card>
-                  <q-card-section>
-                    <p>hello</p>
-                  </q-card-section>
-                </q-card>
-
-              </div>
-              <div class="col">
-                <q-card>
-                  <q-card-section>
-                    <p>Congratulation</p>
-                  </q-card-section>
-                </q-card>
-              </div>
-            </div>
-          </div>
-          <div class="col-9">
-            <div class="column q-gutter-md">
-              <q-card>
-                <q-card-section>
-                  <p>Congratulation</p>
-                </q-card-section>
-              </q-card>
-              <q-card>
-                <q-card-section>
-                  <p>Congratulation</p>
-                </q-card-section>
-              </q-card>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col"></div>
-      <div class="col"></div>
-    </div>
+  <q-page class="row items-center justify-evenly">
+    {{ hello }}
+    <q-btn label="Click me" @click="notify(`${level}`)"></q-btn>
+    <!-- <use-mouse v-slot="{ x, y }"> x: {{ x }}, y: {{ y }} </use-mouse> -->
   </q-page>
 </template>
 
 <script setup lang="ts">
+const hello = ref('hello');
+const { notify } = useQuasar();
+const { level } = useBattery();
 </script>
